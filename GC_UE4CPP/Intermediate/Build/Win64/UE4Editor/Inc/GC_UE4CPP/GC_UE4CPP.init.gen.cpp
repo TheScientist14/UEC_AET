f86438839b1d3d3757a8c219af5a97ded540862d
TheScientist14 <saurytom@gmail.com>
2022-01-18 14:35:05 +0100

    Created Player Character file and BP

--- GC_UE4CPP/Intermediate/Build/Win64/UE4Editor/Inc/GC_UE4CPP/GC_UE4CPP.init.gen.cpp
+++ GC_UE4CPP/Intermediate/Build/Win64/UE4Editor/Inc/GC_UE4CPP/GC_UE4CPP.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeGC_UE4CPP_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xE2808834,
-				0xE6C521F0,
+				0xE24B038E,
+				0x76F87FF3,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
