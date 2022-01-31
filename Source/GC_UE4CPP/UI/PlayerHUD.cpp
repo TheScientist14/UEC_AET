a9a838da7359ee345beb4630096f6f57bc0c22fc
TheScientist14 <saurytom@gmail.com>
2022-01-31 10:49:09 +0100

    HUD should now update

---
+++ Source/GC_UE4CPP/UI/PlayerHUD.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerHUD.h"
+#include "Components/ProgressBar.h"
+#include "Kismet/GameplayStatics.h"
+#include "GC_UE4CPP/Game/MainGameMode.h"
+
+void UPlayerHUD::NativeOnInitialized() {
+	AMainGameMode* GameMode = Cast<AMainGameMode>(UGameplayStatics::GetGameMode(this));
+	GameMode->OnStashedFoodUpdate.AddUObject(this, &UPlayerHUD::UpdateProgressBar);
+}
+
+void UPlayerHUD::UpdateProgressBar(int UpdatedStashedFoodCount, int MaxStashedFoodCount) {
+	ProgressBarWidget->SetPercent((float)UpdatedStashedFoodCount / (float)MaxStashedFoodCount);
+}
