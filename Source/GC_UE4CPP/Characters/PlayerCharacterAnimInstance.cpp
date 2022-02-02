91481b60d54c4170a872f175f1313d00c5181dd3
TheScientist14 <saurytom@gmail.com>
2022-02-02 23:02:21 +0100

    Suppression de BP_Spawner ! Yaaayyyy !!! Added winning and losing animations to player

--- Source/GC_UE4CPP/Characters/PlayerCharacterAnimInstance.cpp
+++ Source/GC_UE4CPP/Characters/PlayerCharacterAnimInstance.cpp
@@ -4,13 +4,17 @@
 #include "PlayerCharacterAnimInstance.h"
 #include "PlayerCharacter.h"
 #include "GC_UE4CPP/Characters/PickUpAbilityComponent.h"
+#include "GC_UE4CPP/Game/MainGameMode.h"
+#include "Kismet/GameplayStatics.h"
 
 UPlayerCharacterAnimInstance::UPlayerCharacterAnimInstance(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 	//set any default values for your variables here
-	//IsPickingUpPuttingDown = false;
-	//Speed = 0;
+	IsPickingUpPuttingDown = false;
+	Speed = 0;
+	HasGameEnded = false;
+	HasWon = false;
 }
 
 void UPlayerCharacterAnimInstance::NativeInitializeAnimation()
@@ -18,15 +22,21 @@ void UPlayerCharacterAnimInstance::NativeInitializeAnimation()
 	Super::NativeInitializeAnimation();
 	Player = Cast<ACharacter>(GetOwningActor());
 	PickUpAbilityComponent = GetOwningActor()->FindComponentByClass<UPickUpAbilityComponent>();
+	AMainGameMode* GameMode = Cast<AMainGameMode>(UGameplayStatics::GetGameMode(this));
+	if (GameMode) {
+		GameMode->OnGameFinished.AddUObject(this, &UPlayerCharacterAnimInstance::PlayAfterGameAnimation);
+	}
 }
 
 void UPlayerCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
 	Super::NativeUpdateAnimation(DeltaSeconds);
 	if (PickUpAbilityComponent) {
-		Speed = Player->GetVelocity().Size();
 		PickedItem = PickUpAbilityComponent->GetPickedUpActor();
 		IsPickingUpPuttingDown = PickUpAbilityComponent->IsPickingUpOrPuttingDown();
+	}
+	if (Player) {
+		Speed = Player->GetVelocity().Size();
 	}
 }
 
@@ -34,4 +44,10 @@ void UPlayerCharacterAnimInstance::NativeUninitializeAnimation() {
 	Super::NativeUninitializeAnimation();
 	Player = nullptr;
 	PickUpAbilityComponent = nullptr;
+}
+
+void UPlayerCharacterAnimInstance::PlayAfterGameAnimation(bool HasGameFinished, bool HasPlayerWon) {
+	GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, "Play");
+	HasGameEnded = HasGameFinished;
+	HasWon = HasPlayerWon;
 }
